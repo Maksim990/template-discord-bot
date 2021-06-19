db159081497da96e034c36921e4b5fc76eaee0d5
Maksim990 <mbartasevis990@gmail.com>
2021-06-19 18:32:37 +0300

    .

---
+++ cmds/test.js
@@ -0,0 +1,7 @@
+﻿module.exports.run = async (bot,message,args) => {
+    let mth = bot.mth;
+    message.channel.send(`Привет: ${mth.toString()}`);
+};
+module.exports.help = {
+    name: "test"
+};
